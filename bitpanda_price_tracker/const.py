12525398af71024e73a0cfda72e89a8eb060a346
Spegeli <19636318+Spegeli@users.noreply.github.com>
2025-02-02 10:29:23 +0100

    Update const.py

--- bitpanda_price_tracker/const.py
+++ bitpanda_price_tracker/const.py
@@ -5,16 +5,17 @@ CONF_UPDATE_INTERVAL = "update_interval"
 
 BITPANDA_API_URL = "https://api.bitpanda.com/v1/ticker"
 DEFAULT_CURRENCY = "EUR"
-CURRENCIES = ["EUR", "USD", "CHF"]
-
-UPDATE_INTERVAL_OPTIONS = {
-    "1": "1 Minute",
-    "2.5": "2.5 Minutes",
-    "5": "5 Minutes"
-}
+CURRENCIES = ["EUR", "USD", "CHF", "GBP", "TRY", "PLN", "HUF", "CZK", "SEK", "DKK"]
 
 CURRENCY_ICONS = {
     "EUR": "mdi:currency-eur",
     "USD": "mdi:currency-usd",
-    "CHF": "mdi:currency-fran"
+    "CHF": "mdi:currency-fra",
+    "GBP": "mdi:currency-gbp",
+    "TRY": "mdi:currency-try",
+    "PLN": "mdi:currency-sign",
+    "HUF": "mdi:currency-sign",
+    "CZK": "mdi:currency-sign",
+    "SEK": "mdi:currency-sign",
+    "DKK": "mdi:currency-sign"
 }
